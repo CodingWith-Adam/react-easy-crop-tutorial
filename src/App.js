fb113e867ce249711a931a039ca11baef47f5b8b
Adam <adam@example.com>
2021-08-15 20:54:01 -0600

    Complete Solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import ImageCropDialog from "./ImageCropDialog";
+
+const initData = [
+  {
+    id: 1,
+    imageUrl: "images/car1.png",
+    croppedImageUrl: null,
+  },
+  {
+    id: 2,
+    imageUrl: "images/car2.png",
+    croppedImageUrl: null,
+  },
+  {
+    id: 3,
+    imageUrl: "images/car3.png",
+    croppedImageUrl: null,
+  },
+  {
+    id: 4,
+    imageUrl: "images/car4.png",
+    croppedImageUrl: null,
+  },
+];
 
 function App() {
+  const [cars, setCars] = useState(initData);
+  const [selectedCar, setSelectedCar] = useState(null);
+
+  const onCancel = () => {
+    setSelectedCar(null);
+  };
+
+  const setCroppedImageFor = (id, crop, zoom, aspect, croppedImageUrl) => {
+    const newCarsList = [...cars];
+    const carIndex = cars.findIndex((x) => x.id === id);
+    const car = cars[carIndex];
+    const newCar = { ...car, croppedImageUrl, crop, zoom, aspect };
+    newCarsList[carIndex] = newCar;
+    setCars(newCarsList);
+    setSelectedCar(null);
+  };
+
+  const resetImage = (id) => {
+    setCroppedImageFor(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {selectedCar ? (
+        <ImageCropDialog
+          id={selectedCar.id}
+          imageUrl={selectedCar.imageUrl}
+          cropInit={selectedCar.crop}
+          zoomInit={selectedCar.zoom}
+          aspectInit={selectedCar.aspect}
+          onCancel={onCancel}
+          setCroppedImageFor={setCroppedImageFor}
+          resetImage={resetImage}
+        />
+      ) : null}
+      {cars.map((car) => (
+        <div className="imageCard" key={car.id}>
+          <img
+            src={car.croppedImageUrl ? car.croppedImageUrl : car.imageUrl}
+            alt=""
+            onClick={() => {
+              console.log(car);
+              setSelectedCar(car);
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/ImageCropDialog.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import Cropper from "react-easy-crop";
+import getCroppedImg from "./cropImage";
+
+const aspectRatios = [
+  { value: 4 / 3, text: "4/3" },
+  { value: 16 / 9, text: "16/9" },
+  { value: 1 / 2, text: "1/2" },
+];
+
+const ImageCropDialog = ({
+  id,
+  imageUrl,
+  cropInit,
+  zoomInit,
+  aspectInit,
+  onCancel,
+  setCroppedImageFor,
+  resetImage,
+}) => {
+  if (zoomInit == null) {
+    zoomInit = 1;
+  }
+  if (cropInit == null) {
+    cropInit = { x: 0, y: 0 };
+  }
+  if (aspectInit == null) {
+    aspectInit = aspectRatios[0];
+  }
+  const [zoom, setZoom] = useState(zoomInit);
+  const [crop, setCrop] = useState(cropInit);
+  const [aspect, setAspect] = useState(aspectInit);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+
+  const onCropChange = (crop) => {
+    setCrop(crop);
+  };
+
+  const onZoomChange = (zoom) => {
+    setZoom(zoom);
+  };
+
+  const onAspectChange = (e) => {
+    const value = e.target.value;
+    const ratio = aspectRatios.find((ratio) => ratio.value == value);
+    setAspect(ratio);
+  };
+
+  const onCropComplete = (croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  };
+
+  const onCrop = async () => {
+    const croppedImageUrl = await getCroppedImg(imageUrl, croppedAreaPixels);
+    setCroppedImageFor(id, crop, zoom, aspect, croppedImageUrl);
+  };
+
+  const onResetImage = () => {
+    resetImage(id);
+  };
+
+  return (
+    <div>
+      <div className="backdrop"></div>
+      <div className="crop-container">
+        <Cropper
+          image={imageUrl}
+          zoom={zoom}
+          crop={crop}
+          aspect={aspect.value}
+          onCropChange={onCropChange}
+          onZoomChange={onZoomChange}
+          onCropComplete={onCropComplete}
+        />
+      </div>
+      <div className="controls">
+        <div className="controls-upper-area">
+          <input
+            type="range"
+            min={1}
+            max={3}
+            step={0.1}
+            value={zoom}
+            onInput={(e) => {
+              onZoomChange(e.target.value);
+            }}
+            className="slider"
+          ></input>
+          <select onChange={onAspectChange}>
+            {aspectRatios.map((ratio) => (
+              <option
+                key={ratio.text}
+                value={ratio.value}
+                selected={ratio.value === aspect.value}
+              >
+                {ratio.text}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="button-area">
+          <button onClick={onCancel}>Cancel</button>
+          <button onClick={onResetImage}>Reset</button>
+          <button onClick={onCrop}>Crop</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageCropDialog;
